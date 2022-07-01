7b65d4e82d1f6e3716c98ab5d01331faa1f6a11a
BK <1209157041@qq.com>
2022-07-01 20:51:59 +0800

    新写的作业

---
+++ src/main.js
@@ -0,0 +1,33 @@
+window.dom = {
+
+    create(string) {
+        const container = document.createElement("template");
+        container.innerHTML = string.trim();
+        return container.content.firstChild
+    },
+    find(Selector, scope) {
+        return (scope || document).querySelectorAll(Selector)
+    },
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                const object = name
+                for (let key in object) {
+
+                    node.style[key] = object[key]
+
+                }
+
+            }
+        }
+    },
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+    },
+}
